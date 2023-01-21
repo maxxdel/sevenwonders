479b9aa63cf03c66aad3916a4d2a36f370dcc85e
Rawmanito <romain.herrenknecht@eleve.isep.fr>
2023-01-21 02:43:26 +0100

    Interface graphique Romain

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.example.sevenwonders {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires com.almasb.fxgl.all;
+
 
     opens com.example.sevenwonders to javafx.fxml;
     exports com.example.sevenwonders;
